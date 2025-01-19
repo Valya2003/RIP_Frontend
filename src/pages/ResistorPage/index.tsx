f17752cc753d329f6f34803fe87b2404bd1a01af
Valya2003 <v9162906483@yandex.ru>
2025-01-19 23:38:06 +0300

    Init

---
+++ src/pages/ResistorPage/index.tsx
@@ -0,0 +1,84 @@
+import * as React from 'react';
+import {useParams} from "react-router-dom";
+import {useEffect, useState} from "react";
+import {T_Resistor} from "src/modules/types.ts";
+import {Col, Container, Row} from "reactstrap";
+import {ResistorMocks} from "src/modules/mocks.ts";
+import mockImage from "assets/mock.png";
+
+type Props = {
+    selectedResistor: T_Resistor | null,
+    setSelectedResistor: React.Dispatch<React.SetStateAction<T_Resistor | null>>,
+    isMock: boolean,
+    setIsMock: React.Dispatch<React.SetStateAction<boolean>>
+}
+
+const ResistorPage = ({selectedResistor, setSelectedResistor, isMock, setIsMock}: Props) => {
+    const { id } = useParams<{id: string}>();
+
+    const fetchData = async () => {
+        try {
+            const env = await import.meta.env;
+            const response = await fetch(`${env.VITE_API_URL}/api/resistors/${id}`)
+            const data = await response.json()
+            setSelectedResistor(data)
+        } catch {
+            createMock()
+        }
+    }
+
+    const createMock = () => {
+        setIsMock(true)
+        setSelectedResistor(ResistorMocks.find(resistor => resistor?.id == parseInt(id as string)) as T_Resistor)
+    }
+
+    const [image, setImage] = useState<string>()
+
+    const fetchImage = async () => {
+        const env = await import.meta.env;
+        const response = await fetch(`${env.VITE_API_URL}/api/resistors/${id}/image`)
+        const blob = await response.blob()
+        const url = URL.createObjectURL(blob)
+        setImage(url)
+    }
+
+    useEffect(() => {
+        if (!isMock) {
+            fetchData()
+            fetchImage()
+        } else {
+            createMock()
+        }
+
+        return () => setSelectedResistor(null)
+    }, []);
+
+    if (!selectedResistor) {
+        return (
+            <div>
+
+            </div>
+        )
+    }
+
+    return (
+        <Container>
+            <Row>
+                <Col md="6">
+                    <img
+                        alt=""
+                        src={isMock ? mockImage as string : image}
+                        className="w-100"
+                    />
+                </Col>
+                <Col md="6">
+                    <h1 className="mb-3">{selectedResistor.name}</h1>
+                    <p className="fs-5">Описание: {selectedResistor.description}</p>
+                    <p className="fs-5">Сопротивление: {selectedResistor.resistance} Ом</p>
+                </Col>
+            </Row>
+        </Container>
+    );
+};
+
+export default ResistorPage
---
+++ src/pages/ResistorsListPage/index.tsx
@@ -0,0 +1,81 @@
+import {Button, Col, Container, Form, Input, Row} from "reactstrap";
+import ResistorCard from "components/ResistorCard";
+import {ChangeEvent, FormEvent, useEffect} from "react";
+import * as React from "react";
+import {useAppSelector} from "src/store/store.ts";
+import {updateResistorName} from "src/store/slices/resistorsSlice.ts";
+import {T_Resistor} from "modules/types.ts";
+import {ResistorMocks} from "modules/mocks.ts";
+import {useDispatch} from "react-redux";
+
+type Props = {
+    resistors: T_Resistor[],
+    setResistors: React.Dispatch<React.SetStateAction<T_Resistor[]>>
+    isMock: boolean,
+    setIsMock: React.Dispatch<React.SetStateAction<boolean>>
+}
+
+const ResistorsListPage = ({resistors, setResistors, isMock, setIsMock}:Props) => {
+
+    const dispatch = useDispatch()
+
+    const {resistor_name} = useAppSelector((state) => state.resistors)
+
+    const handleChange = (e:ChangeEvent<HTMLInputElement>) => {
+        dispatch(updateResistorName(e.target.value))
+    }
+
+    const createMocks = () => {
+        setIsMock(true)
+        setResistors(ResistorMocks.filter(resistor => resistor.name.toLowerCase().includes(resistor_name.toLowerCase())))
+    }
+
+    const handleSubmit = async (e:FormEvent) => {
+        e.preventDefault()
+        await fetchResistors()
+    }
+
+    const fetchResistors = async () => {
+        try {
+            const env = await import.meta.env;
+            const response = await fetch(`${env.VITE_API_URL}/api/resistors/?resistor_name=${resistor_name.toLowerCase()}`)
+            const data = await response.json()
+            setResistors(data.resistors)
+            setIsMock(false)
+        } catch {
+            createMocks()
+        }
+    }
+
+    useEffect(() => {
+        fetchResistors()
+    }, []);
+
+    return (
+        <Container>
+            <Row className="mb-5">
+                <Col md="6">
+                    <Form onSubmit={handleSubmit}>
+                        <Row>
+                            <Col xs="8">
+                                <Input value={resistor_name} onChange={handleChange} placeholder="Поиск..."></Input>
+                            </Col>
+                            <Col>
+                                <Button color="primary" className="w-100 search-btn">Поиск</Button>
+                            </Col>
+                        </Row>
+                    </Form>
+                </Col>
+            </Row>
+            <Row>
+                {resistors?.map(resistor => (
+                    <Col key={resistor.id} sm="12" md="6" lg="4">
+                        <ResistorCard resistor={resistor} isMock={isMock} />
+                    </Col>
+                ))}
+            </Row>
+        </Container>
+    );
+};
+
+export default ResistorsListPage
